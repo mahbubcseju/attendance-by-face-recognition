c8f50d6174361458cd3a0a9007dc2f58a94951f6
urmi6750 <fariyahossainurmi@gmail.com>
2020-04-15 23:38:33 +0600

    added registration.css file

---
+++ attendance/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fariya/attendance/attendance-by-face-recognition/attendance/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
