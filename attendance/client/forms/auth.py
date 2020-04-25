9b571bb38892062eec5bcab9eabeb06e00851062
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-04-25 20:04:16 +0900

    form refactoring

--- attendance/client/forms/auth.py
+++ attendance/client/forms/auth.py
@@ -17,7 +17,34 @@ class SignUpForm(UserCreationForm):
             'password2',
         ]
 
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    username = forms.CharField(
+        label='Username',
+        required=True,
+        widget=forms.TextInput(attrs={'size': 40}),
+    )
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        help_text='Required. Inform a valid email address.'
+    )
+    first_name = forms.CharField(
+        label='First Name',
+        required=True,
+        widget=forms.TextInput(attrs={'size': 40}),
+    )
+    last_name = forms.CharField(
+        label='Last Name',
+        required=True,
+        widget=forms.TextInput(attrs={'size': 40}),
+    )
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+    )
+    password2 = forms.CharField(
+        label='Password Confirmation',
+        widget=forms.PasswordInput(),
+    )
 
 
 class LoginForm(forms.Form):
