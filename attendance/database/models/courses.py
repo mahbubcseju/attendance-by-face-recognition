8046f848ae532f44cd4564d697ff58e04fc12413
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-04-29 17:36:19 +0900

    Added user to the table

--- attendance/database/models/courses.py
+++ attendance/database/models/courses.py
@@ -6,9 +6,17 @@ class Course(models.Model):
     name = models.CharField('Course Name', max_length=128)
     supervisor = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def get_students_info(self):
+    def get_all_students_info(self):
         students = self.coursestudent_set.all()
         return students
+
+    def get_joined_students_info(self):
+        students = self.coursestudent_set.filter(status__lte=1)
+        return students
+
+    def get_recent_periods(self):
+        periods = self.attendanceperiod_set.all().order_by('-start_at')[:10]
+        return periods
 
 
 class CourseStudent(models.Model):
