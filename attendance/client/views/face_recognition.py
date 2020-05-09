068a092ff9e9bdfda70e0a86a92eac7cd77c3ce2
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-05-09 15:23:22 +0900

    added post after image upload

--- attendance/client/views/face_recognition.py
+++ attendance/client/views/face_recognition.py
@@ -12,5 +12,12 @@ class RegisterImage(FormView):
     form_class = forms.RegisterImageForm
     success_url = reverse_lazy('Home')
 
-    # def post(self, request, username, *args, **kwargs):
-
+    def post(self, request, username, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        print(form)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            return self.form_valid(form)
+        return self.form_invalid(form)
