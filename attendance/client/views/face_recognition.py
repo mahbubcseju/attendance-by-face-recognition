60de38ae218c72e352143164bce1fea727967377
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-05-15 13:50:54 +0900

    base64 image uploaded

--- attendance/client/views/face_recognition.py
+++ attendance/client/views/face_recognition.py
@@ -1,22 +1,45 @@
+import re
+import base64
+import json
+
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.urls import reverse_lazy, reverse
 from django.views.generic import FormView, TemplateView
 from django.utils.decorators import method_decorator
+from django.core.files.base import ContentFile
 
 from client import forms
+from database.models import FaceImageBase
 
 
 @method_decorator(login_required, name='dispatch')
-class RegisterImage(FormView):
+class RegisterImage(TemplateView):
     template_name = 'client/register_image.html'
-    form_class = forms.RegisterImageForm
     success_url = reverse_lazy('Home')
 
     def post(self, request, username, *args, **kwargs):
-        form = self.form_class(request.POST, request.FILES)
-        if form.is_valid():
-            obj = form.save(commit=False)
-            obj.user = request.user
-            obj.save()
-            return self.form_valid(form)
-        return self.form_invalid(form)
+        try:
+            imageData = request.POST.get('image')
+            dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+            imageData = dataUrlPattern.match(imageData).group(2)
+            file = base64.b64decode(imageData)
+            image = ContentFile(file, '{}.jpeg'.format(username))
+            FaceImageBase.objects.create(user=request.user, image=image)
+            obj = {
+                'status': 'OK',
+                'result': reverse('Home'),
+            }
+        except Exception as e:
+            obj = {
+                'status': 'NG',
+                'result': str(e),
+            }
+
+        res = HttpResponse(
+            json.dumps(obj, ensure_ascii=False, indent=2),
+            content_type='application/json',
+        )
+        if obj['status'] == 'NG':
+            res.status_code = 500
+        return res
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mahbubcseju/Desktop/projects/works/attendance-by-face-recognition/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
